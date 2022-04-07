23a12ed10633b8f958ac531220b001aa9f9ec59b
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2022-04-08 01:46:19 +0700

    Custom View (Button and EditText)
    Latihan Membuat Custom Button dan EditText

---
+++ MyCustomView/app/src/main/java/com/dicoding/picodiploma/mycustomview/MyButton.kt
@@ -0,0 +1,61 @@
+package com.dicoding.picodiploma.mycustomview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.Gravity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+
+class MyButton : AppCompatButton {
+
+    private lateinit var enabledBackground: Drawable
+    private lateinit var disabledBackground: Drawable
+    private var txtColor: Int = 0
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    /**
+     * mengubah bentuk button
+     **/
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        background = if(isEnabled) enabledBackground else disabledBackground
+
+        setTextColor(txtColor)
+        textSize = 12f
+        gravity = Gravity.CENTER
+        text = if(isEnabled) "Submit" else "Isi Dulu"
+    }
+
+    private fun init() {
+        txtColor = ContextCompat.getColor(context, android.R.color.background_light)
+        enabledBackground = ContextCompat.getDrawable(context, R.drawable.bg_button) as Drawable
+        disabledBackground = ContextCompat.getDrawable(context, R.drawable.bg_button_disable) as Drawable
+    }
+}
+
+/*
+setBackground
+setTextColor
+setTextSize
+setGravity
+setText
+setHint
+setInputType
+addTextChangeListener
+setOnTouchListener
+getText
+dll
+*/
