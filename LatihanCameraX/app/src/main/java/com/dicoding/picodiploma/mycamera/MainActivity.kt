e3b24e74c1298adbe10cff61d622527d17ee5129
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2022-04-29 01:43:24 +0700

    latihan intent gallery

--- LatihanCameraX/app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
+++ LatihanCameraX/app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
@@ -2,6 +2,7 @@ package com.dicoding.picodiploma.mycamera
 
 import android.Manifest
 import android.content.Intent
+import android.content.Intent.ACTION_GET_CONTENT
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -79,7 +80,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startGallery() {
-        Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+        val intent = Intent()
+        intent.action = ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
     }
 
     private fun startTakePhoto() {
@@ -133,6 +138,16 @@ class MainActivity : AppCompatActivity() {
             )
 
             binding.previewImageView.setImageBitmap(result)
+        }
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+            val myFile = uriToFile(selectedImg, this@MainActivity)
+            binding.previewImageView.setImageURI(selectedImg)
         }
     }
 
