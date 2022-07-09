1e004a09d15ba5d7816d976b1319f5c8f9d6aaa9
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2022-07-09 15:46:05 +0700

    shared element example

---
+++ android_navigation_shared_elements-master/app/src/main/java/com/example/sergiobelda/navigationsharedelements/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.sergiobelda.navigationsharedelements.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sergiobelda.navigationsharedelements.R
+import com.example.sergiobelda.navigationsharedelements.ui.recyclerviewexample.dummy.DummyContent
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
